f9656c8aebe4306f15040eda327d08c85a820c1a
AkshayReddy777 <92278314+AkshayReddy777@users.noreply.github.com>
2023-01-11 19:07:55 -0500

    Update bashfile.sh

--- bashfile.sh
+++ bashfile.sh
@@ -1,1 +1,17 @@
-
+######################################
+#!/bin/bash
+sudo apt update
+sudo apt install git curl -y
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+source ~/.bashrc   
+nvm install 14.21.2
+sudo add-apt-repository universe -y
+sudo apt install g++ build-essential git curl python2-minimal -y
+curl https://install.meteor.com/ | sh
+npm install --global yarn
+git clone https://github.com/AkshayReddy777/RocketChat.git
+cd RocketChat
+git checkout develop
+yarn
+yarn dsv
+#########################################
